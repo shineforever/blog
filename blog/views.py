25b559a06431b5227baff1fb40a176decfb33721
shineforever <shine_forever@yeah.net>
2016-04-21 18:55:28 +0800

    第一章练习完结，增加分页和list_view 使用

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from django.shortcuts import render,get_object_or_404
+from django.views.generic import ListView
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
@@ -18,6 +19,12 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,slug=post,
