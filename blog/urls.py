2e534f7f6350921d65aafcc342350caf0187aff2
shineforever <shine_forever@yeah.net>
2016-04-21 18:01:00 +0800

    first

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# coding:utf-8
+from django.conf.urls import url
+from . import views
+"""
+Created on: 2016-04-19 16:50 
+@author: guolt
+"""
+
+urlpatterns = [
+    # post views
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+    r'(?P<post>[-\w]+)/$',
+    views.post_detail,
+    name='post_detail'),
+]
+
