ed5294f3169e12427b200cdef1e5dc95ef6cd9dc
shineforever <shine_forever@yeah.net>
2016-05-06 15:29:59 +0800

    修复评论功能

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@
 # coding:utf-8
 
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -9,4 +10,10 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name','email','body')
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
+
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
@@ -35,10 +36,21 @@ def post_detail(request,year,month,day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+    return render(request,'blog/post/detail.html',{'post':post,
+                                                    'comments':comments,
+                                                    'comment_form':comment_form})
+
+
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status='published')
@@ -66,6 +78,10 @@ def post_share(request,post_id):
     return render(request,'blog/post/share.html',{'post':post,
                                                   'form':form,
                                                   'sent':sent})
+
+
+
+
 
 
 
